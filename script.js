a6f61d6f8eeacae011d4766ad88de9e1963b1086
Christina L <clee04@baker.edu>
2017-02-20 08:48:54 -0500

    lab 11 group

--- script.js
+++ script.js
@@ -1,72 +1,62 @@
-$(function() {
+$(function(){
 
-var postData = {};
-$.get('https://www.reddit.com/r/aww/.json', function(data) {
-console.log(data);
+  var postData = {};
 
-postData = data;
-var redditInfo = postData.data.children;
-for(var i = 0; i < 10; i++){
+  $.get('https://www.reddit.com/r/aww/.json', function(data){
+    console.log(data);
+    postData = data;
+
+    for (var i = 0; i<10; i++){
+     var info = postData.data.children[i].data;
+
+       $("#container").append('<div class="post">'
+      + '<img src="' + info.thumbnail + '">'
+      + '<p class="title">' + info.title + '</p>'
+      + '<p class="author">' + info.author + '</p>'
+      + '<p class="comments">' + info.num_comments + " comments" + '</p>'
+      + '</div>');
 
 
-var redImage = $
-var redTitles = $('<h3>').text(redditInfo[i].data.title);
-$('body').append(redTitles);
-var redAuthor = $('<p>').text(redditInfo[i].data.author);
-$('body').append(redAuthor);
-var redComments = $('<p>').text(redditInfo[i].data.num_comments);
-$('body').append(redComments);
-// var redSubmit = $('<p>').text(redditInfo[i]).data.created);
-// $('body').append(redSubmit);
 
-// responseBody.data.children.forEach(function(post){
-//   var redTitle = post.data.title;
-//   var redAuthor = post.data.author;
-//   var redSubmit = post.data.submit;
-//   var redComments = post.data.num_comments;
-// });
-// $('body').append($('<h2>').text(responseBody.data.children[1].data.title))
-//($'body').append.(redTitles) $('body').append($('<div>').append($('<span>').text(responseBody.data.children[1].data.author)))
-// $('body').append($('<p>').text(responseBody.data.children[1].data.submit)
-// });
-// responseBody.data.children.forEach(function(post) {
-// post.data.title;
-// post.data.author;
-// })
+               }
+
+
+
+  });
+
+});
+
+// console.log(info.title);
+// console.log(info.author);
+// console.log(info.thumbnail) thumbnail=picture img src;
+// console.log(info.num_comments);
+
+// NOTE: wrote a simpler for loop to better organize and see the process removed sumbit info and added "i" to be looped
+// var title = []
+//  responseBody.data.children[2].data.title
+//  console.log(title);
 //
-// responseBody.data.children.forEach(function(post) {
-//   // var redTitle = post.data.title
-
-
-// responseBody.data.children.forEach(function(post) {
-// $('body').append($('<div>').append($('<h2>').text(responseBody.title)))
-
-
-
-
-
-// })
-
-
-            // var redTitle = responseBody.data.title;
-            // var redAuthor = responseBody.data.author;
-            // var redSubmit = responseBody.data.submit;
-            // var redComments = responseBody.data.num_comments;
-
-
-            // //   $('body').append($('<div>').addClass('postName').append($('<h2>').text(posts.title)))
-            // // // })];
-            // $('body').append($('<div>').append($('<h2>').text(redditPosts.title)))
-            // // });
-            //   // var title = post.data.title;
-
-
-        // });
-}
-});
-});
-
-
-
-
-// });
+//              $.get('https://www.reddit.com/r/aww/.json').done(function(responseBody){
+//                console.log(responseBody.data.children.data).forEach(function(post){
+//                post.data.title;
+//                post.data.author;
+//                post.data.submit;
+//                post.data.num_comments;
+// $(function() {
+//
+// var postData = {};
+// $.get('https://www.reddit.com/r/aww/.json', function(data) {
+// console.log(data);
+//
+// postData = data;
+// var redditInfo = postData.data.children;
+// for(var i = 0; i < 10; i++){
+//
+//
+// var redImage =
+// var redTitles = $('<h1>').text(redditInfo[i].data.title);
+// $('body').append(redTitles);
+// var redAuthor = $('<p>').text(redditInfo[i].data.author);
+// $('body').append(redAuthor);
+// var redComments = $('<p>').text(redditInfo[i].data.num_comments);
+// $(‘body’).append(redComments);  } }); });
